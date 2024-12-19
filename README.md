baeb42dfcd5b3f0015f4097b6cdb073401e2200e
PuP <165062521+bmallexander@users.noreply.github.com>
2024-12-19 08:37:19 +0000

    Core

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# famdash
+# FamDash
+
+Does you're company needs a reliable hosting & staff management dashboard ?
+
+# Introducing FamDash
+
+We have been starting development as of December 2024. We do not have any ETA, But here are some features.
+
+# Features
+
+- Connect to any Dedicated or VPS Server.
+
+- Manage all roles and permissions.
+
+- Manage and monitor your resources.
+
+- Integrate with other tools.
+
+- More to come.
---
+++ users/views.py
@@ -0,0 +1,66 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from .forms import RegisterForm, LoginForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False  # Deactivate account until email confirmation
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            # Email verification
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account'
+            message = render_to_string('email_verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+            return render(request, 'registration_success.html')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('home')  # Redirect to the home page after activation
+    else:
+        return render(request, 'activation_invalid.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after login
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
